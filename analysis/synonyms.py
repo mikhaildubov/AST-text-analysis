d34cd879e05805a19d094007addb3c16485cd2d9
Mikhail Dubov <msdubov@gmail.com>
2014-05-24 10:33:46 +0400

    Output synonym lists in analysis.synonyms instead of pairs

--- analysis/synonyms.py
+++ analysis/synonyms.py
@@ -14,14 +14,9 @@ def main(args):
     print "Prepared synonimizer\n"
 
     synonym_dicts = synonimizer.get_synonyms(threshold=0.3, return_similarity_measure=True)
-    synonym_tuples = []
     for w1 in synonym_dicts:
-        for w2, sim in synonym_dicts[w1]:
-            synonym_tuples.append((w1, w2, sim))
-    synonym_tuples.sort(key=lambda (w1, w2, sim): -sim)
-    for w1, w2, sim in synonym_tuples:
-        print "%.2f %s <-> %s" % (sim, w1, w2)
-
+        syn_str = ["%s (%.2f)" % (w2, sim) for w2, sim in synonym_dicts[w1]]
+        print "%s -> %s" % (w1, ", ".join(syn_str))
 
 if __name__ == "__main__":
     main(sys.argv[1:])
