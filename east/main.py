2bbbbec4b12d2ccd344139c7ff8d84f6f4fe1e0f
Mikhail Dubov <msdubov@gmail.com>
2014-06-06 01:51:02 +0400

    Change CLI interface for different graph formats
    Instead of the '-g' option (for drawing in GML) there is now the '-f' option
    that stands for 'format' and accepts the format name as its argument, e.g.:
    
        east -f gml keyphrases graph ...
    
    Supported formats now are 'gml' and 'edges' and can be extended in future.

--- east/main.py
+++ east/main.py
@@ -11,11 +11,12 @@ from east import utils
 
 def main():
     args = sys.argv[1:]
-    opts, args = getopt.getopt(args, "a:l:t:sdg")
+    opts, args = getopt.getopt(args, "a:f:l:t:ds")
     opts = dict(opts)
-    opts.setdefault("-a", "easa")
-    opts.setdefault("-l", "0.6")
-    opts.setdefault("-t", "0.25")
+    opts.setdefault("-a", "easa")   # Algorithm to use for computing ASTs
+    opts.setdefault("-f", "edges")  # Graph output format (also "gml" possible)
+    opts.setdefault("-l", "0.6")    # Level of significance for graph construction
+    opts.setdefault("-t", "0.25")   # Threshold of the matching score
 
     if len(args) < 2:
         print "Invalid syntax."
@@ -31,6 +32,7 @@ def main():
         use_synonyms = "-s" in opts
         normalized_scores = "-d" not in opts
         ast_algorithm = opts["-a"]
+        graph_format = opts["-f"]
         significance_level = float(opts["-l"])
         score_threshold = float(opts["-t"])
 
@@ -59,14 +61,15 @@ def main():
 
             keyphrases_table = applications.keyphrases_table(keyphrases, texts, ast_algorithm,
                                                              normalized_scores, synonimizer)
-            res = u""
+            res = u"<table>"
             for keyphrase in sorted(keyphrases_table.keys()):
-                res += u'<keyphrase value="%s">\n' % keyphrase
+                res += u'  <keyphrase value="%s">\n' % keyphrase
                 for text in sorted(keyphrases_table[keyphrase].keys()):
-                    res += u'  <text name="%s">' % text
+                    res += u'    <text name="%s">' % text
                     res += u'%.3f' % keyphrases_table[keyphrase][text]
                     res += u'</text>\n'
-                res += u'</keyphrase>\n'
+                res += u'  </keyphrase>\n'
+            res += u"</table>\n"
 
             print res.encode("utf-8", "ignore")
 
@@ -75,14 +78,17 @@ def main():
             graph = applications.keyphrases_graph(keyphrases, texts, significance_level,
                                                   score_threshold, ast_algorithm,
                                                   normalized_scores, synonimizer)
-            print_gml = "-g" in opts
-            if print_gml:
+            if graph_format == "gml":
                 res = utils.graph2gml(graph)
-            else:
+            elif graph_format == "edges":
                 res = u""
                 for node in graph:
                     if graph[node]:
                         res += "%s -> %s\n" % (node, ", ".join(graph[node]))
+            else:
+                print ("Unknown graph format: '%s'. "
+                       "Please use one of: 'gml', 'edges'." % graph_format)
+                return 1
 
             print res.encode("utf-8", "ignore")
 
